abd093fdf7a4d8a9df045165fe358715e5a80a97
BMS <85956989+bloomstarbms@users.noreply.github.com>
2026-07-25 01:36:53 +0100

    v0.8.0: wire cascade into scheduler

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import { checkRevival } from './sources/dex/revival.js';
 import { pollCex } from './sources/cex/monitor.js';
 import { pollFunding } from './sources/cex/funding.js';
 import { pollAnnouncements } from './sources/cex/announcements.js';
+import { pollCascade } from './sources/cex/cascade.js';
 import { checkWhales } from './sources/chain/whale.js';
 import { checkConfluence } from './core/confluence.js';
 import { pollCpi } from './sources/calendar/cpi.js';
@@ -67,7 +68,7 @@ async function heartbeat() {
 }
 
 async function pollAll() {
-  await Promise.allSettled([pollDex(), pollCex(), pollFunding(), pollAnnouncements(), pollCpi(), pollEvents()]);
+  await Promise.allSettled([pollDex(), pollCex(), pollFunding(), pollCascade(), pollAnnouncements(), pollCpi(), pollEvents()]);
   await checkOutcomes().catch(() => {});
   await heartbeat().catch(() => {});
 }
